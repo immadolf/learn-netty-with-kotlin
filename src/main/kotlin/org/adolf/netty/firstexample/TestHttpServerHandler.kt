c9f5781df7d0623401742d48cf63c1f1e84a56a9
adolf <fqluowy@gmail.com>
2020-06-18 21:47:35 +0800

    netty回调与channel执行流程

--- src/main/kotlin/org/adolf/netty/firstexample/TestHttpServerHandler.kt
+++ src/main/kotlin/org/adolf/netty/firstexample/TestHttpServerHandler.kt
@@ -6,6 +6,7 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
 import io.netty.handler.codec.http.*
 import io.netty.util.CharsetUtil
+import java.net.URI
 
 /**
  * 自定义的处理器
@@ -18,7 +19,20 @@ class TestHttpServerHandler : SimpleChannelInboundHandler<HttpObject>() {
 
     // 读取客户端发过来的请求，并向客户端返回响应
     override fun channelRead0(ctx: ChannelHandlerContext?, msg: HttpObject?) {
+
+        println(msg!!.javaClass)
+
+        println(ctx!!.channel().remoteAddress())
         if (msg is HttpRequest) {
+
+            println("请求方法名：${msg.method().name()}")
+
+            val uri = URI(msg.uri())
+            if ("/favicon.ico".equals(uri.path)) {
+                println("请求favicon.ico")
+                return
+            }
+
             // 返回给客户端的内容
             val content: ByteBuf = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8)
 
@@ -31,6 +45,33 @@ class TestHttpServerHandler : SimpleChannelInboundHandler<HttpObject>() {
 
             // 向客户端返回响应
             ctx!!.writeAndFlush(response)
+            ctx.channel().close()
         }
+    }
+
+
+    override fun channelActive(ctx: ChannelHandlerContext?) {
+        println("channel active")
+        super.channelActive(ctx)
+    }
+
+    override fun channelRegistered(ctx: ChannelHandlerContext?) {
+        println("channel registered")
+        super.channelRegistered(ctx)
+    }
+
+    override fun handlerAdded(ctx: ChannelHandlerContext?) {
+        println("handler added")
+        super.handlerAdded(ctx)
+    }
+
+    override fun channelInactive(ctx: ChannelHandlerContext?) {
+        println("channel inactive")
+        super.channelInactive(ctx)
+    }
+
+    override fun channelUnregistered(ctx: ChannelHandlerContext?) {
+        println("channel unregistered")
+        super.channelUnregistered(ctx)
     }
 }
