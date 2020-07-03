f7bb1838b20b2b8236f39a2726bd3c11c0a27c9a
adolf <fqluowy@gmail.com>
2020-07-03 22:21:30 +0800

    - 对字符集的一些说明

--- src/main/kotlin/org/adolf/nio/NioTest13.kt
+++ src/main/kotlin/org/adolf/nio/NioTest13.kt
@@ -19,6 +19,10 @@ fun main() {
     val outputChannel = outputRandomAccessFile.channel
     val mappedByteBuffer = inputChannel.map(FileChannel.MapMode.READ_ONLY, 0, inputLength)
 
+    //当解码和编码使用同一种字符集时，即使和源文件的字符集不同，也不会造成字节的丢失，
+    //因此只要最终选择一个合适的字符集来显示，就不会乱码
+    //例如NioTest13_in.txt的字符编码是UTF8，即使这里代码中使用ISO-8859-1，只要上下编解码的字符集一样，
+    //最终生成的NioTest13_out.txt还是可以在UTF8字符集下正确的显示内容
     val decoder = Charsets.UTF_8.newDecoder()
     val encoder = Charsets.UTF_8.newEncoder()
     val charBuffer = decoder.decode(mappedByteBuffer)
